95b5dc5cfee1e494328d96979019a45fb367ad43
carolgreene <carolgreene3@gmail.com>
2020-03-24 16:21:04 -0700

    added basic controller code

--- app/controllers/api/v1/shows_controller.rb
+++ app/controllers/api/v1/shows_controller.rb
@@ -1,2 +1,51 @@
 class ShowsController < ApplicationController
+
+  def index 
+    @shows = Show.all 
+  end
+
+  def show 
+  end
+
+  def create 
+    @show = Show.new(show_params)
+
+    if @show.save
+      render json:#add serializer info
+    else
+      error_resp = {
+        error: @show.errors.full_messages.to_sentence
+      }
+      render json: error_resp, status: :unprocessable_entity
+    end
+  end
+
+  def update 
+    if @show.update(show_params)
+      render json: #add serializer info
+    else
+       error_resp = {
+         error: @show.errors.full_messages.to_sentence
+       }
+       render json: error_resp, status: unprocessable_entity
+    end
+  end
+
+  def destroy
+    @shows = Show.all
+    @show.destroy
+    render json: #add serializer info
+  end
+
+  private
+  
+  def set_show
+    @show = Show.find(params[:id])
+  end
+
+  def show_params 
+    params.require(:show).permit(:name, :genre, :summary)
+  end
+
+
 end
