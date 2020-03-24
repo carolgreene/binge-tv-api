5d7b2526fde158e9eb8f120d4c743db9730d7131
carolgreene <carolgreene3@gmail.com>
2020-03-24 15:29:01 -0700

    added attributes & ran migrations

--- db/migrate/20200324014304_create_shows.rb
+++ db/migrate/20200324014304_create_shows.rb
@@ -1,6 +1,9 @@
 class CreateShows < ActiveRecord::Migration[5.2]
   def change
     create_table :shows do |t|
+      t.string :name
+      t.string :genre
+      t.string :summary
 
       t.timestamps
     end
