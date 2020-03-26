88e2fa3b4a159f54891488609af08155c955d5be
carolgreene <carolgreene3@gmail.com>
2020-03-25 18:57:46 -0700

    basic controller code

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,32 @@
-class UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
+
+  def index 
+    @users = User.all 
+    render json: @users 
+  end
+
+  def show 
+  end
+
+  def create 
+    @user = User.new(user_params)
+  end
+
+  def edit 
+  end
+
+  def update 
+  end
+
+  private 
+
+  def find_user 
+    @user = User.find_by(id: params[:id])
+  end 
+
+  def user_params 
+    params.require(:user).permit(:email, :password)
+  end
+       
+
 end
