b387de511d10c872cd730feecdf047c1d1061ba7
carolgreene <carolgreene3@gmail.com>
2020-03-30 17:05:28 -0700

    added serializer gem & code

--- Gemfile
+++ Gemfile
@@ -15,7 +15,8 @@ gem 'puma', '~> 3.11'
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
  gem 'bcrypt', '~> 3.1.7'
-
+#use FastJsonApi to serialize data
+gem 'fast_jsonapi'
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
--- app/controllers/api/v1/shows_controller.rb
+++ app/controllers/api/v1/shows_controller.rb
@@ -1,47 +1,49 @@
 class Api::V1::ShowsController < ApplicationController
 
   def index 
-    @shows = Show.all 
-    render json: @shows
+    shows = Show.all 
+    render json: ShowSerializer.new(shows)
   end
 
   def show 
+    show = Show.find(params[:id])
+    render json: ShowSerializer.new(show)
   end
 
   def create 
-    @show = Show.new(show_params)
+    show = Show.new(show_params)
 
-    if @show.save
-      render json: "add serializer"
+    if show.save
+      render json: ShowSerializer.new(show)
     else
       error_resp = {
-        error: @show.errors.full_messages.to_sentence
+        error: show.errors.full_messages.to_sentence
       }
       render json: error_resp, status: :unprocessable_entity
     end
   end
 
   def update 
-    if @show.update(show_params)
-      render json: "add serializer"
+    if show.update(show_params)
+      render json: ShowSerializer.new(show)
     else
        error_resp = {
-         error: @show.errors.full_messages.to_sentence
+         error: show.errors.full_messages.to_sentence
        }
        render json: error_resp, status: unprocessable_entity
     end
   end
 
   def destroy
-    @shows = Show.all
-    @show.destroy
-    render json: "add serializer"
+    shows = Show.all
+    show.destroy
+    render json: ShowSerializer.new(shows)
   end
 
   private
   
   def set_show
-    @show = Show.find(params[:id])
+    show = Show.find(params[:id])
   end
 
   def show_params 
