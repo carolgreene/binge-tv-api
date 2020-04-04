da273892d983d5c58b8e070e5ed1787cc8ff89ad
carolgreene <carolgreene3@gmail.com>
2020-04-04 14:09:14 -0700

    show listing works

--- binge-tv-frontend/src/index.js
+++ binge-tv-frontend/src/index.js
@@ -1,13 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
-    listShows()
+    fetchShows()
 })
 
-function listShows() {
+function fetchShows() {
+    let shows = document.getElementById("shows")
+    let li = document.createElement("li")
   fetch(`http://10.0.0.99:3000/api/v1/shows`) 
   .then((res) => res.json())
   .then(results => {
-     results.data.forEach(show => {
-        console.log(show)
-     })
+     results.data.forEach(show => listShow(show)
+        
+     )
   })
+}
+
+function listShow(show) {
+  let div = document.getElementById("shows")
+  let newLi = document.createElement("li")
+  newLi.innerText = show.attributes.name
+  console.log(show)
+  div.appendChild(newLi)
+  
 }
