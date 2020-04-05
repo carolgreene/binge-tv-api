376099e079feebe3bf7435b4532487cab479d744
carolgreene <carolgreene3@gmail.com>
2020-04-05 16:02:10 -0700

    added fetch & div for users

--- binge-tv-frontend/src/index.js
+++ binge-tv-frontend/src/index.js
@@ -17,8 +17,20 @@ function fetchShows() {
 function listShow(show) {
   let div = document.getElementById("shows")
   let newLi = document.createElement("li")
-  newLi.innerText = show.attributes.name
+  newLi.innerText = show.attributes.name   /*`${show.attributes.name}-${show.attributes.genre}`*/
   console.log(show)
   div.appendChild(newLi)
   
+}
+
+function fetchUsers() {
+  let users = document.getElementById("users")
+  let li = document.createElement("li")
+fetch(`http://10.0.0.99:3000/api/v1/users`) 
+.then((res) => res.json())
+.then(results => {
+   results.data.forEach(user => console.log(user)
+      
+   )
+})
 }
