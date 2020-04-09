9a57a1f7a798a882b2653a61372a2174459cf0dc
carolgreene <carolgreene3@gmail.com>
2020-04-08 17:06:05 -0700

    added user list

--- binge-tv-frontend/src/index.js
+++ binge-tv-frontend/src/index.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetchShows()
+  fetchShows()
+  fetchUsers()  
 })
 
 function fetchShows() {
@@ -15,22 +16,28 @@ function fetchShows() {
 }
 
 function listShow(show) {
-  let div = document.getElementById("shows")
+  let showsDiv = document.getElementById("shows")
   let newLi = document.createElement("li")
   newLi.innerText = show.attributes.name   /*`${show.attributes.name}-${show.attributes.genre}`*/
   console.log(show)
-  div.appendChild(newLi)
+  showsDiv.appendChild(newLi)
   
 }
 
 function fetchUsers() {
   let users = document.getElementById("users")
   let li = document.createElement("li")
-fetch(`http://10.0.0.99:3000/api/v1/users`) 
-.then((res) => res.json())
-.then(results => {
-   results.data.forEach(user => console.log(user)
-      
-   )
-})
+  fetch(`http://10.0.0.99:3000/api/v1/users`) 
+  .then((res) => res.json())
+  .then(results => {
+    results.data.forEach(user => listUsers(user)      
+    )
+  })
+}
+
+function listUsers(user) {
+  let usersDiv = document.getElementById("users")
+  let newLi = document.createElement("li")
+  newLi.innerText = user.attributes.email
+  usersDiv.appendChild(newLi)
 }
