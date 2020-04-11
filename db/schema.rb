4bb9c443e6d6f2d3c55f7fa97d344f0f8861cf6f
carolgreene <carolgreene3@gmail.com>
2020-04-11 15:02:15 -0700

    dropped users schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_24_014437) do
+ActiveRecord::Schema.define(version: 2020_04_11_215810) do
 
   create_table "shows", force: :cascade do |t|
     t.string "name"
     t.string "genre"
     t.string "summary"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
